c4af7bb49fb40b75c9b8940ac160f25ac644c204
Baran Tas <45711329+baran-tas@users.noreply.github.com>
2025-03-20 00:53:27 +0000

    Update currentDB.sql

--- Database/currentDB.sql
+++ Database/currentDB.sql
@@ -2,6 +2,7 @@
 CREATE TABLE Users (
     id INTEGER PRIMARY KEY AUTOINCREMENT, --Automatic ids
     username TEXT NOT NULL UNIQUE, --Username based on some criterion
+    --Could take name and last name
     email TEXT NOT NULL UNIQUE, --Takes email, could also add a field for phone number
     password TEXT NOT NULL, --Hash the password then store it
     account_type TEXT CHECK(account_type IN ('personal', 'professional')) NOT NULL, --Determines the account type
