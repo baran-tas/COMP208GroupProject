6df63d2559183e9dbc137f4334a5e2f9e13aee76
Baran Tas <45711329+baran-tas@users.noreply.github.com>
2025-03-20 00:52:06 +0000

    Create currentDB.sql

---
+++ Database/currentDB.sql
@@ -0,0 +1,9 @@
+--Table to manage users
+CREATE TABLE Users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, --Automatic ids
+    username TEXT NOT NULL UNIQUE, --Username based on some criterion
+    email TEXT NOT NULL UNIQUE, --Takes email, could also add a field for phone number
+    password TEXT NOT NULL, --Hash the password then store it
+    account_type TEXT CHECK(account_type IN ('personal', 'professional')) NOT NULL, --Determines the account type
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP --Saves the timestamp at which the account was created, why not?
+);
