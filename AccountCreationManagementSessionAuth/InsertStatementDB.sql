3e87278a55fd5418861205658d992f8c4c431275
Baran Tas <45711329+baran-tas@users.noreply.github.com>
2025-03-25 18:19:15 +0000

    DB Insert Statements

---
+++ AccountCreationManagementSessionAuth/InsertStatementDB.sql
@@ -0,0 +1,26 @@
+INSERT INTO Users (id, username, email, password, account_type)
+VALUES
+  (1, 'store1', 'email1@store.com', 'password1', 'professional'),
+  (2, 'store2', 'email2@store.com', 'password2', 'professional'),
+  (3, 'store3', 'email3@store.com', 'password3', 'professional'),
+  (4, 'store4', 'email4@store.com', 'password4', 'professional'),
+  (5, 'store5', 'email5@store.com', 'password5', 'professional'),
+  (6, 'store6', 'email6@store.com', 'password6', 'professional'),
+  (7, 'store7', 'email7@store.com', 'password7', 'professional'),
+  (8, 'store8', 'email8@store.com', 'password8', 'professional'),
+  (9, 'store9', 'email9@store.com', 'password9', 'professional'),
+  (10, 'store10', 'email10@store.com', 'password10', 'professional');
+
+
+INSERT INTO Professional (id, tax_id, store_name, store_description, phone, address, establishment_type, establishment_website)
+VALUES
+  (1, 'taxid1', 'Coffee Shop', 'Cozy cafe serving coffee and pastries', '02011588811', '231 Coffee Shop Avenue', 'Cafe', 'https://coffeeshop.co.uk'),
+  (2, 'taxid2', 'Burger Place', 'Modern fast food burger joint', '02049498172', '546 Buger Place', 'Fast Food', 'https://burgerplace.co.uk'),
+  (3, 'taxid3', 'Pizza Palace', 'Hot, fast, and tasty pizza', '02038392914', '723 Pizza Lane', 'Restaurant', 'https://pizzapalace.co.uk'),
+  (4, 'taxid4', 'Taco Town', 'Street style tacos', '02093504003', '429 Taco City', 'Food Truck', 'https://tacotown.co.uk'),
+  (5, 'taxid5', 'Sandwich Shop', 'Quick and tasty sandwiches', '02052432881', '582 Sandwich Corner', 'Cafe', 'https://sandwichshop.co.uk'),
+  (6, 'taxid6', 'Chinese Food Place', 'Fresh traditional Chinese food', '02040797165', '312 China Town', 'Restaurant', 'https://chinesefoodplace.co.uk'),
+  (7, 'taxid7', 'Pasta Place', 'Great, fresh pasta', '02077849621', '289 Pasta Road', 'Restaurant', 'https://pastaplace.co.uk'),
+  (8, 'taxid8', 'Doughnut Shop', 'Doughnuts, doughnuts, and doughnuts', '02067370931', '689 Fried Goods Lane', 'Fast Food', 'https://doughnutshop.co.uk'),
+  (9, 'taxid9', 'Salad Bar', 'Healthy salads and vegan food', '02023816087', '143 Vegan Food Street', 'Cafe', 'https://saladbar.co.uk'),
+  (10, 'taxid10', 'Texan BBQ', 'Smoked Texan meat and BBQ', '02034829695', '594 Texas Lane', 'Fast Food', 'https://texanbbq.co.uk');
