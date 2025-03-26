09298fe257640bc689fab322400e80e1a659eeda
adam-andrews <andrewsadam07@gmail.com>
2025-03-26 19:44:07 +0000

    Connected redux store

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+import store from '@/redux/store';
+
+let persistor = persistStore(store);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+		<Provider store={store}>
+			<PersistGate persistor={persistor}>
+				<Component {...pageProps} />
+			</PersistGate>
+		</Provider>
+	);
 }
