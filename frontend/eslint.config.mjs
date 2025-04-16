36a0446d9c08d0effd51cda51fd7d308290e17ac
adam-andrews <andrewsadam07@gmail.com>
2025-04-16 13:17:49 +0100

    configured es-lint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { off } from "process";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next','next/core-web-vitals','next/typescript'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      '@typescript-eslint/no-require-imports':'off',
+      '@typescript-eslint/no-explicit-any':'off'
+
+    },
+  }),];
 
 export default eslintConfig;
