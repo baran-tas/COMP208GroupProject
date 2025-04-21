4f2b7b800479c14d39773d2831d82fa6703a8de0
adam-andrews <andrewsadam07@gmail.com>
2025-04-21 16:14:39 +0100

    added navbar for stores

--- frontend/pages/[store].tsx
+++ frontend/pages/[store].tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { createClient } from '@supabase/supabase-js'
+import NavBar from '@/components/navbar';
 
 
 
@@ -33,6 +34,7 @@ function StorePage({orders }:any) {
 	
 	return (
 		<div className="bg-white">
+			<div><NavBar /></div>
 		<div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
 		  <h2 className="sr-only">Products</h2>
   
